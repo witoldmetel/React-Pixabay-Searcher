a06139c1b49ffbd9b74122202432b41556908ad9
Managersky <witoldmetel@gmail.com>
2018-05-15 15:15:12 +0200

    finish app

--- src/components/ImageResults.js
+++ src/components/ImageResults.js
@@ -7,43 +7,68 @@ import Dialog from 'material-ui/Dialog';
 import FlatButton from 'material-ui/FlatButton';
 
 export default class ImageResults extends Component {
-  render() {
-    let imageListContent;
-    const { images } = this.props;
-
-    if(images) {
-        imageListContent = (
-            <GridList cols={3}>
-                {images.map(img => (
-                    <GridTile
-                        title={img.tags}
-                        key={img.id}
-                        subtitle={
-                            <span>
-                                by <strong>{img.user}</strong>
-                            </span>
-                        }
-                        actionIcon={
-                            <IconButton>
-                                <ZoomIn color="white" />
-                            </IconButton>
-                        }
-                    >
-                        <img src={img.largeImageURL} alt={img.tags}/>
-                    </GridTile>
-                ))}
-            </GridList>
-        )
-    } else {
-        imageListContent = null;
+    state = {
+        open: false,
+        currentImg: ''
     }
 
-    return (
-      <div>
-        {imageListContent}
-      </div>
-    )
-  }
+    handleOpen = img => {
+        this.setState({ open: true, currentImg: img });
+    }
+
+    handleClose = img => {
+        this.setState({ open: false, currentImg: img });
+    }
+
+    render() {
+        let imageListContent;
+        const { images } = this.props;
+
+        if(images) {
+            imageListContent = (
+                <GridList cols={4}>
+                    {images.map(img => (
+                        <GridTile
+                            title={img.tags}
+                            key={img.id}
+                            subtitle={
+                                <span>
+                                    by <strong>{img.user}</strong>
+                                </span>
+                            }
+                            actionIcon={
+                                <IconButton onClick={() => this.handleOpen(img.largeImageURL)}>
+                                    <ZoomIn color="white" />
+                                </IconButton>
+                            }
+                        >
+                            <img src={img.largeImageURL} alt={img.tags}/>
+                        </GridTile>
+                    ))}
+                </GridList>
+            )
+        } else {
+            imageListContent = null;
+        }
+
+        const actions = [
+            <FlatButton label="Close" primary={true} onClick={this.handleClose} />
+        ]
+
+        return (
+        <div>
+            {imageListContent}
+            <Dialog
+                actions={actions}
+                modal={false}
+                open={this.state.open}
+                onRequestClose={this.handleClose}
+            >
+                <img src={this.state.currentImg} alt="" style={{ width: '100%' }}/>
+            </Dialog>
+        </div>
+        )
+    }
 }
 
 ImageResults.propTypes = {
