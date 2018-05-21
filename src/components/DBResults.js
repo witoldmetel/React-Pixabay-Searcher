b17a101d78913c87f8c58631fe2e453602d44629
witoldmetel <witoldmetel@gmail.com>
2018-05-21 16:20:04 +0200

    update

---
+++ src/components/DBResults.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+
+export default class DBResults extends Component {
+    constructor() {
+        super();
+
+        this.state = (
+            {
+                db: []
+            }
+        );
+    }
+
+    displayData() {
+        fetch("")
+    }
+
+    render() {
+        return (
+        <div>
+
+        </div>
+        )
+    }
+}
